4427e92325d21ed20a06540fba0f39ed0a919562
mazfahim <52876279+MazFahim@users.noreply.github.com>
2024-11-19 09:17:48 +0600

    about and contact us page integrated

--- TwenryFirstCenturyAutoParts/commonPages/urls.py
+++ TwenryFirstCenturyAutoParts/commonPages/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('contact_us/', views.contact_us, name='contact_us'),
+    path('about/', views.about, name='about'),
 ]
--- TwenryFirstCenturyAutoParts/commonPages/views.py
+++ TwenryFirstCenturyAutoParts/commonPages/views.py
@@ -6,3 +6,13 @@ from django.template import loader
 def home(request):
     template = loader.get_template('home.html')
     return HttpResponse(template.render())
+
+
+def contact_us(request):
+    template = loader.get_template('contact_us.html')
+    return HttpResponse(template.render())
+
+
+def about(request):
+    template = loader.get_template('about.html')
+    return HttpResponse(template.render())
