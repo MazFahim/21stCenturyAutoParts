b17808fe244ea917f5257c0e80d0bdb5544b6a39
mazfahim <52876279+MazFahim@users.noreply.github.com>
2024-12-24 12:16:49 +0600

    Update

--- TwenryFirstCenturyAutoParts/commonPages/models.py
+++ TwenryFirstCenturyAutoParts/commonPages/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class UsedSparePartsOptions(models.Model):
+    option_id = models.AutoField(primary_key=True)
+    option_title = models.CharField(max_length=255)
+    option_url = models.URLField()
+
+    def __str__(self):
+        return self.option_title
--- TwenryFirstCenturyAutoParts/commonPages/views.py
+++ TwenryFirstCenturyAutoParts/commonPages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.template import loader
+from .models import *
 
 
 def home(request):
@@ -16,3 +17,12 @@ def contact_us(request):
 def about(request):
     template = loader.get_template('about.html')
     return HttpResponse(template.render())
+
+def about(request):
+    template = loader.get_template('car_services.html')
+    return HttpResponse(template.render())
+
+#Used Spare Parts function
+def get_spare_parts_options(request):
+    options = UsedSparePartsOptions.objects.all().values('option_title', 'option_url')
+    return JsonResponse(list(options), safe=False)
