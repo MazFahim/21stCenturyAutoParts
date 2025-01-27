28629c8c4532b2f1b057f7fcd954c502a1a0aa10
mazfahim <52876279+MazFahim@users.noreply.github.com>
2025-01-27 16:51:17 +0600

    update

--- TwenryFirstCenturyAutoParts/commonPages/admin.py
+++ TwenryFirstCenturyAutoParts/commonPages/admin.py
@@ -2,3 +2,4 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(UsedSparePartsOptions)
+admin.site.register(UsedCars)
--- TwenryFirstCenturyAutoParts/commonPages/models.py
+++ TwenryFirstCenturyAutoParts/commonPages/models.py
@@ -7,3 +7,17 @@ class UsedSparePartsOptions(models.Model):
 
     def __str__(self):
         return self.option_title
+    
+
+class UsedCars(models.Model):
+    used_cars_id = models.AutoField(primary_key=True)
+    used_car_title = models.CharField(max_length=100)
+    milage = models.IntegerField()
+    description = models.TextField()
+
+
+class SparePart(models.Model):
+    spare_part_id = models.AutoField(primary_key=True)
+    spare_part_title = models.CharField(max_length=100)
+    milage = models.IntegerField()
+    description = models.TextField()
