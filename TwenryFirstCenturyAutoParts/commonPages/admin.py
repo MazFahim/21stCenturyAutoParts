5949a5e8021abc308954121165928e030258b288
mazfahim <52876279+MazFahim@users.noreply.github.com>
2025-02-03 17:12:35 +0600

    used cars image upload update

--- TwenryFirstCenturyAutoParts/commonPages/admin.py
+++ TwenryFirstCenturyAutoParts/commonPages/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import *
+from django.utils.html import format_html
+
+
+class UsedCarsAdmin(admin.ModelAdmin):
+    list_display = ('used_car_title', 'milage', 'image_preview')
+    def image_preview(self, obj):
+        if obj.image1:
+            return format_html('<img src="{}" width="50" height="50" style="border-radius:5px;"/>', obj.image1.url)
+        return "(No Image)"
 
 admin.site.register(UsedSparePartsOptions)
-admin.site.register(UsedCars)
+admin.site.register(UsedCars, UsedCarsAdmin)
--- TwenryFirstCenturyAutoParts/commonPages/models.py
+++ TwenryFirstCenturyAutoParts/commonPages/models.py
@@ -14,6 +14,11 @@ class UsedCars(models.Model):
     used_car_title = models.CharField(max_length=100)
     milage = models.IntegerField()
     description = models.TextField()
+    image1 = models.ImageField(upload_to='used_cars_images/', blank=True, null=True)
+    image2 = models.ImageField(upload_to='used_cars_images/', blank=True, null=True)
+    image3 = models.ImageField(upload_to='used_cars_images/', blank=True, null=True)
+    image4 = models.ImageField(upload_to='used_cars_images/', blank=True, null=True)
+    image5 = models.ImageField(upload_to='used_cars_images/', blank=True, null=True)
 
 
 class SparePart(models.Model):
