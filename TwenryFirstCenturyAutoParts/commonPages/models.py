825ed9f49709ebfd959df89fffdddc5ab8c0525e
mazfahim <52876279+MazFahim@users.noreply.github.com>
2025-02-04 09:06:55 +0600

    used cars model update

--- TwenryFirstCenturyAutoParts/commonPages/models.py
+++ TwenryFirstCenturyAutoParts/commonPages/models.py
@@ -14,15 +14,16 @@ class UsedCars(models.Model):
     used_car_title = models.CharField(max_length=100)
     milage = models.IntegerField()
     description = models.TextField()
-    image1 = models.ImageField(upload_to='used_cars_images/', blank=True, null=True)
-    image2 = models.ImageField(upload_to='used_cars_images/', blank=True, null=True)
-    image3 = models.ImageField(upload_to='used_cars_images/', blank=True, null=True)
-    image4 = models.ImageField(upload_to='used_cars_images/', blank=True, null=True)
-    image5 = models.ImageField(upload_to='used_cars_images/', blank=True, null=True)
+    mainImage = models.ImageField(upload_to='used_cars_images/', blank=True, null=True)
+    rightSideView = models.ImageField(upload_to='used_cars_images/', blank=True, null=True)
+    leftSideView = models.ImageField(upload_to='used_cars_images/', blank=True, null=True)
+    interiorView = models.ImageField(upload_to='used_cars_images/', blank=True, null=True)
+    backSideImage = models.ImageField(upload_to='used_cars_images/', blank=True, null=True)
+    extraImage = models.ImageField(upload_to='used_cars_images/', blank=True, null=True)
 
 
 class SparePart(models.Model):
     spare_part_id = models.AutoField(primary_key=True)
     spare_part_title = models.CharField(max_length=100)
-    milage = models.IntegerField()
     description = models.TextField()
+    extraImage = models.ImageField(upload_to='used_cars_images/', blank=True, null=True)
