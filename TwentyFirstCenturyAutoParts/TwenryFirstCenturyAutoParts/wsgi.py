c527371a5fdb3ab8a9e7af6b441d005c7f44e7fd
Maz Fahim <mazfahim0@gmail.com>
2025-11-18 18:42:40 +0600

    update

--- TwentyFirstCenturyAutoParts/TwenryFirstCenturyAutoParts/wsgi.py
+++ TwentyFirstCenturyAutoParts/TwenryFirstCenturyAutoParts/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for TwenryFirstCenturyAutoParts project.
+WSGI config for TwentyFirstCenturyAutoParts project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TwenryFirstCenturyAutoParts.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TwentyFirstCenturyAutoParts.settings')
 
 application = get_wsgi_application()
