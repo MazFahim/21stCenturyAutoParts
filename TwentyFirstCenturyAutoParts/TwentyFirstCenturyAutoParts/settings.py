5762f2bb5293c61ace384a74e7620161ffd39396
Maz Fahim <mazfahim0@gmail.com>
2025-11-20 14:23:48 +0600

    update

--- TwentyFirstCenturyAutoParts/TwentyFirstCenturyAutoParts/settings.py
+++ TwentyFirstCenturyAutoParts/TwentyFirstCenturyAutoParts/settings.py
@@ -11,11 +11,12 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import dj_database_url, os
+import dj_database_url, os, sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
